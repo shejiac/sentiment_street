568d74d66c505aae8895ddb5a6735d5249785fde
shejiac <cshejia@gmail.com>
2025-07-07 13:39:11 +0800

    Update reddit_scraper.py
    added check_for_async=False

--- scraper/reddit_scraper.py
+++ scraper/reddit_scraper.py
@@ -24,6 +24,7 @@ reddit = praw.Reddit(
     client_id=os.getenv("REDDIT_CLIENT_ID"),
     client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
     user_agent=os.getenv("REDDIT_USER_AGENT")
+    check_for_async=False
 )
 
 # ==========================
