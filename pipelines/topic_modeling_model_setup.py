c7ce7d23ee14a6d043e2ce14f4acbc77b1918c44
Nikola Tran <nghtrn.work@gmail.com>
2025-08-06 12:28:16 +0700

    Modified comments at top of the file

--- pipelines/topic_modeling_model_setup.py
+++ pipelines/topic_modeling_model_setup.py
@@ -1,4 +1,5 @@
-# Run this script to train a topic classification model using logistic regression.
+# Run this script to set up a topic modeling model.
+# Current method of set up: training a logistic regression model
 # Run this script from the root directory of the project.
 import json
 import pandas as pd
